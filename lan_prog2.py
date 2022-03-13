357ff4c63ac979cc6b7c024db267f5a41b61d749
Programming641 <56218301+Programming641@users.noreply.github.com>
2022-03-13 12:42:11 +0900

    Add files via upload

--- lan_prog2.py
+++ lan_prog2.py
@@ -2,7 +2,9 @@ from lib.process_sentence import Sentence_Processor
 
    
    
-sentence = "Most people eat two or three meals in a day."
+#sentence = "Most people eat two or three meals in a day."
+
+sentence = "There are some key phrases that are useful to learn in English in order to explain your daily routine to another person."
 
 
 Sentence_Processor.process_sentence(sentence)
